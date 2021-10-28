6b8912ce010ba1ad5cb8fb6788a320dcbb11243c
Nathan Nicholson <85798301+TheNathanN@users.noreply.github.com>
2021-10-27 19:17:06 -0500

    Set searchMode to false

--- context/app-state.js
+++ context/app-state.js
@@ -3,7 +3,7 @@ import AppContext from './app-context';
 
 const AppState = props => {
   const [offsetVal, setOffsetVal] = useState(0); // Controls which tokens are fetched from api
-  const [searchMode, setSearchMode] = useState(true); // Should be false as default
+  const [searchMode, setSearchMode] = useState(false); // Should be false as default
   const [selectedCoin, setSelectedCoin] = useState();
 
   return (
