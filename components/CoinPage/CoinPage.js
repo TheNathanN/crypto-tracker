a7f436982fe7a80ab19a472af129daa2069c05ab
Nathan Nicholson <85798301+TheNathanN@users.noreply.github.com>
2021-10-30 11:42:13 -0500

    Add import for NewsInfo styling and prep CoinPage

---
+++ components/CoinPage/CoinPage.js
@@ -0,0 +1,11 @@
+import styles from './CoinPage.module.scss';
+
+const CoinPage = () => {
+  return (
+    <div className={styles['container']}>
+      <div></div>
+    </div>
+  );
+};
+
+export default CoinPage;
---
+++ pages/[coin-name].js
@@ -0,0 +1,9 @@
+const Coin = () => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Coin;
