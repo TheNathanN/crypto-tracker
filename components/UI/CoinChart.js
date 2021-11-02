074a9901803d81419105977bc1f2f94ed15c971c
Nathan Nicholson <85798301+TheNathanN@users.noreply.github.com>
2021-11-02 11:22:51 -0500

    Set up CoinChart to recieve data

--- components/UI/CoinChart.js
+++ components/UI/CoinChart.js
@@ -1,18 +1,55 @@
+import { useEffect, useState } from 'react';
+import { formatPrice } from '../../hooks/customHooks';
 import { Line } from 'react-chartjs-2';
 
 import styles from './CoinChart.module.scss';
 
 const CoinChart = props => {
-  const { coinHistory } = props;
+  // Destructure Props
+  const data = props?.coinHistory?.history;
 
-  const data = 14;
+  // Local State
+  const [timeLabels, setTimeLabels] = useState();
+  const [priceLabels, setPriceLabels] = useState();
 
-  console.log(coinHistory);
+  // Function to create fill labels array
+  const createTimeLabels = data => {
+    const date = new Date(data);
+    const dateString = date.toLocaleDateString('en-US');
+    const timeString = date.toLocaleTimeString('en-US');
+    return `${dateString}, ${timeString}`;
+  };
+
+  useEffect(() => {
+    // Local Arrays for Chart Labels & Data
+    const timeLabelsData = [];
+    const priceLabelsData = [];
+
+    // Loop through the data and push it into the local arrays above.
+    if (data) {
+      for (let i = 0; i <= data?.length; i++) {
+        const timeData = data[i]?.timestamp;
+        const priceData = data[i]?.price;
+        if (timeData) {
+          timeLabelsData.push(createTimeLabels(timeData));
+        }
+        if (priceData) {
+          priceLabelsData.push(formatPrice(priceData));
+        }
+      }
+    }
+
+    // Set the Local Array Data into the Local State
+    setTimeLabels(timeLabelsData);
+    setPriceLabels(priceLabelsData);
+  }, [data, setTimeLabels]);
+
+  console.log(timeLabels);
 
   return (
     <div>
       <div>Coin Chart</div>
-      {/* <Line /> */}
+      {/* { <Line height={200} width={400}  />} */}
     </div>
   );
 };
