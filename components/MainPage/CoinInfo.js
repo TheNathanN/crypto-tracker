2dd74d32e5a9c4b68626fd880b3333666f88e375
nathan <thenathannicholson@gmail.com>
2023-01-15 15:32:31 -0800

    Change Image tag to an img tag in CoinInfo

--- components/MainPage/CoinInfo.js
+++ components/MainPage/CoinInfo.js
@@ -1,42 +1,42 @@
-import Image from 'next/image';
-import { useRouter } from 'next/router';
-import { formatPrice, formatTicker } from '../../helpers/helperFunctions';
+import Image from "next/image"
+import { useRouter } from "next/router"
+import { formatPrice, formatTicker } from "../../helpers/helperFunctions"
 
-import styles from './CoinInfo.module.scss';
+import styles from "./CoinInfo.module.scss"
 
-const CoinInfo = props => {
-  const router = useRouter();
+const CoinInfo = (props) => {
+  const router = useRouter()
 
   // Import Props
-  const { coinInfo } = props;
-  const { uuid, rank, name, symbol, price, change, iconUrl } = coinInfo;
+  const { coinInfo } = props
+  const { uuid, rank, name, symbol, price, change, iconUrl } = coinInfo
 
   // onClick Functions
   const clickHandler = () => {
-    router.push(`/${uuid}`);
-  };
+    router.push(`/${uuid}`)
+  }
 
   return (
-    <div className={styles['container']} onClick={clickHandler}>
-      <section className={styles['coin-info']}>
-        <div className={styles['rank']}>{rank}</div>
-        <div className={styles['image']}>
-          <Image src={iconUrl} width={20} height={20} alt={name} />
+    <div className={styles["container"]} onClick={clickHandler}>
+      <section className={styles["coin-info"]}>
+        <div className={styles["rank"]}>{rank}</div>
+        <div className={styles["image"]}>
+          <img src={iconUrl} width={20} height={20} alt={name} />
         </div>
-        <div className={styles['name']}>{name}</div>
-        <div className={styles['ticker']}>{formatTicker(symbol)}</div>
+        <div className={styles["name"]}>{name}</div>
+        <div className={styles["ticker"]}>{formatTicker(symbol)}</div>
       </section>
-      <section className={styles['price-info']}>
-        {change < 0 && <div className={styles['down']}>{change}%</div>}
-        {change >= 0 && <div className={styles['up']}>{change}%</div>}
-        <div className={styles['price']}>{`$${
+      <section className={styles["price-info"]}>
+        {change < 0 && <div className={styles["down"]}>{change}%</div>}
+        {change >= 0 && <div className={styles["up"]}>{change}%</div>}
+        <div className={styles["price"]}>{`$${
           formatPrice(price) > 999.99
             ? Number(formatPrice(price)).toLocaleString()
             : formatPrice(price)
         }`}</div>
       </section>
     </div>
-  );
-};
+  )
+}
 
-export default CoinInfo;
+export default CoinInfo
